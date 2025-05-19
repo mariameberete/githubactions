198060e2f150fca9c4d047cc01cb10fdc40b5628
mariameberete <mariame.berete@wolterskluwer.com>
2025-05-19 11:03:33 +0000

    terraform setup

--- main.tf
+++ main.tf
@@ -1,18 +1,11 @@
-    terraform {
-        backend "local" {
-        # The name of your Terraform Cloud organization.
-        organization = "example-organization"
-
-        # The name of the Terraform Cloud workspace to store Terraform state files in.
-        workspaces {
-            name = "example-workspace"
-            }
-        }
-    }
+ terraform {
+       backend "local" {
+       }
+     }
 
     # An example resource that does nothing.
     resource "null_resource" "example" {
-    triggers = {         
+        triggers = {
             value = "A example resource that does nothing!"
         }
     }
